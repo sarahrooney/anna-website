9212c2f5ab3cd8703142b57b783c96a4340b0c47
ennu0 <rooney.sarah5@yahoo.com>
2017-04-18 17:00:27 -0500

    added hover functionality to logos

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,27 @@ var exitMobileMenu = document.querySelector('#exit');
 exitMobileMenu.addEventListener ("click", function(){
 	mobileMenuExpand.style.display = "none"
 });
+
+
+var hpNameLogo = document.querySelector('#namegraphic');
+var hpFaceLogo = document.querySelector('#facelogo');
+
+hpNameLogo.addEventListener ("mouseover", function(){
+	hpNameLogo.setAttribute("src","images/face_logo_170px.png");
+	hpFaceLogo.setAttribute("src","images/ar_logo_70px.png");
+})
+
+hpNameLogo.addEventListener ("mouseout", function(){
+	hpNameLogo.setAttribute("src","images/ar_logo_170px.png");
+	hpFaceLogo.setAttribute("src","images/face_logo.png");
+})
+
+hpFaceLogo.addEventListener ("mouseover", function(){
+	hpNameLogo.setAttribute("src","images/face_logo_170px.png");
+	hpFaceLogo.setAttribute("src","images/ar_logo_70px.png");
+})
+
+hpFaceLogo.addEventListener ("mouseout", function(){
+	hpNameLogo.setAttribute("src","images/ar_logo_170px.png");
+	hpFaceLogo.setAttribute("src","images/face_logo.png");
+})
