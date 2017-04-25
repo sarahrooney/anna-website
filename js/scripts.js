6eee4e10b8856c970306a4cb35839306735403b7
ennu0 <rooney.sarah5@yahoo.com>
2017-04-25 14:30:30 -0500

    added exit button for mobile menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,126 @@
+//HOMEPAGE MOBILE MENU
+var mobileMenuIcon = document.querySelector('#mobilemenu');
 var mobileMenuExpand = document.querySelector('#mobilemenuexpand');
-var expanded = document.querySelector('#expanded');
-var sub = document.querySelector('#sub');
-var mobileMenu = document.querySelector('#mobilemenu');
 
-mobileMenu.addEventListener ("click", function(){
+mobileMenuIcon.addEventListener ("click", function(){
 	mobileMenuExpand.style.display = "flex"
 });
 
-var exitMobileMenu = document.querySelector('#exit');
+var exitMobileMenu = document.querySelector('#exitmobilemenu');
 
 exitMobileMenu.addEventListener ("click", function(){
-	mobileMenuExpand.style.display = "none"
+ 	mobileMenuExpand.style.display = "none";
 });
 
 
+//CURRENT SECTION LINK IMAGE
+/*
+	when a menu link is active, or when scrollY's value is corresponds to a section,
+	give that link's div a ".currentsection" class
+*/
+// /*element*/.setAttribute('class', 'currentsection')
+
+
+
+//HOMEPAGE LOGOS
 var hpNameLogo = document.querySelector('#namegraphic');
 var hpFaceLogo = document.querySelector('#facelogo');
 
 hpNameLogo.addEventListener ("mouseover", function(){
 	hpNameLogo.setAttribute("src","images/face_logo_170px.png");
 	hpFaceLogo.setAttribute("src","images/ar_logo_70px.png");
-})
+});
 
 hpNameLogo.addEventListener ("mouseout", function(){
 	hpNameLogo.setAttribute("src","images/ar_logo_170px.png");
 	hpFaceLogo.setAttribute("src","images/face_logo.png");
-})
+});
 
 hpFaceLogo.addEventListener ("mouseover", function(){
 	hpNameLogo.setAttribute("src","images/face_logo_170px.png");
 	hpFaceLogo.setAttribute("src","images/ar_logo_70px.png");
-})
+});
 
 hpFaceLogo.addEventListener ("mouseout", function(){
 	hpNameLogo.setAttribute("src","images/ar_logo_170px.png");
 	hpFaceLogo.setAttribute("src","images/face_logo.png");
-})
-
-
-
-// ABOUT SECTION
-
-var abtMobileMenu = document.querySelector('#abt_menuexpand');
-var abtMobileMenuExpanded = document.querySelector('#abt_mobilemenuexpand');
-
-abtMobileMenu.addEventListener ("click", function(){
-	abtMobileMenuExpanded.style.display = "flex"
 });
+
+
+
+// ABOUT SECTION MOBILE MENU
+// var abtMobileMenu = document.querySelector('#abt_menuexpand');
+// var abtMobileMenuExpanded = document.querySelector('#abt_mobilemenuexpand');
+
+// abtMobileMenu.addEventListener ("click", function(){
+// 	abtMobileMenuExpanded.style.display = "flex"
+// });
+
+
+
+//SCROLLING
+// window.scrollY value from homepage to about me = 0 to 814
+
+/*
+	if 'about me' button is clicked,
+	window.scrollY increments from its current number
+	to a value of 814
+
+playbutton.addEventListener("click", function(e){
+
+	var titlescreen = document.getElementById('titlescreen');
+	var i = 1;
+		var fade = setInterval(function(){
+			playbutton.style.opacity = i;
+			titlescreen.style.opacity = i;
+			i -= 0.05;
+
+			if (i <= 0) {
+				clearInterval(fade)
+				playbutton.style.display = "none";
+				titlescreen.style.display = "none";
+			}
+		}, 50)
+	});
+*/
+
+var abtmeButton = document.querySelector('.aboutme')
+
+var currentPosition = window.scrollY
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
