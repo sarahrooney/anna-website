451be2e7eaee2a2dde8298645eb1f14b267761c0
ennu0 <rooney.sarah5@yahoo.com>
2017-04-24 19:43:50 -0500

    responsive layout for 800px and above is done.

--- js/scripts.js
+++ js/scripts.js
@@ -36,3 +36,14 @@ hpFaceLogo.addEventListener ("mouseout", function(){
 	hpNameLogo.setAttribute("src","images/ar_logo_170px.png");
 	hpFaceLogo.setAttribute("src","images/face_logo.png");
 })
+
+
+
+// ABOUT SECTION
+
+var abtMobileMenu = document.querySelector('#abt_menuexpand');
+var abtMobileMenuExpanded = document.querySelector('#abt_mobilemenuexpand');
+
+abtMobileMenu.addEventListener ("click", function(){
+	abtMobileMenuExpanded.style.display = "flex"
+});
