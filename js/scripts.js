ae074a5c0380e7da9229e1fe65684ed265636f5a
ennu0 <rooney.sarah5@yahoo.com>
2017-04-25 17:25:00 -0500

    fixed menu to top of page

--- js/scripts.js
+++ js/scripts.js
@@ -26,22 +26,22 @@ exitMobileMenu.addEventListener ("click", function(){
 var hpNameLogo = document.querySelector('#namegraphic');
 var hpFaceLogo = document.querySelector('#facelogo');
 
-hpNameLogo.addEventListener ("mouseover", function(){
+hpNameLogo.addEventListener("mouseover", function(){
 	hpNameLogo.setAttribute("src","images/face_logo_170px.png");
 	hpFaceLogo.setAttribute("src","images/ar_logo_70px.png");
 });
 
-hpNameLogo.addEventListener ("mouseout", function(){
+hpNameLogo.addEventListener("mouseout", function(){
 	hpNameLogo.setAttribute("src","images/ar_logo_170px.png");
 	hpFaceLogo.setAttribute("src","images/face_logo.png");
 });
 
-hpFaceLogo.addEventListener ("mouseover", function(){
+hpFaceLogo.addEventListener("mouseover", function(){
 	hpNameLogo.setAttribute("src","images/face_logo_170px.png");
 	hpFaceLogo.setAttribute("src","images/ar_logo_70px.png");
 });
 
-hpFaceLogo.addEventListener ("mouseout", function(){
+hpFaceLogo.addEventListener("mouseout", function(){
 	hpNameLogo.setAttribute("src","images/ar_logo_170px.png");
 	hpFaceLogo.setAttribute("src","images/face_logo.png");
 });
@@ -61,10 +61,52 @@ hpFaceLogo.addEventListener ("mouseout", function(){
 //SCROLLING
 // window.scrollY value from homepage to about me = 0 to 814
 
+var aboutMe = document.querySelector('#aboutme')
+var homepage = document.querySelector('#homepage')
+var desktopMenuSection = document.querySelector('#desktop_menu_section')
+var desktopMenuLink = document.querySelector('#menu a:link')
+
+var upperLeftLogo = document.querySelector('#facelogo')
+
+
+window.addEventListener("scroll", function(){
+	// console.log(window.scrollY);
+
+	if (window.scrollY >= homepage.offsetTop && window.scrollY < aboutMe.offsetTop) {
+	
+		desktopMenuSection.style.backgroundColor = "#E8DBD2"
+		desktopMenuLink.style.color = "#E8DBD2"
+
+		upperLeftLogo.setAttribute('src','images/face_logo.png')
+
+		console.log('homepage');
+	
+	} else if (window.scrollY >= aboutMe.offsetTop) {
+	
+		desktopMenuSection.style.backgroundColor = "#E8E8E0"
+		desktopMenuLink.style.color = "#E8E8E0"
+
+		upperLeftLogo.setAttribute('src','images/ar_logo_70px.png')
+
+		console.log('about me');
+	}
+})
+
+
+
+
+
 /*
 	if 'about me' button is clicked,
 	window.scrollY increments from its current number
 	to a value of 814
+
+
+
+
+
+
+
 
 playbutton.addEventListener("click", function(e){
 
@@ -83,11 +125,6 @@ playbutton.addEventListener("click", function(e){
 		}, 50)
 	});
 */
-
-var abtmeButton = document.querySelector('.aboutme')
-
-var currentPosition = window.scrollY
-
 
 
 
