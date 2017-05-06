a4cbedfed6fe0adb7399449017fd54f075be9d06
ennu0 <rooney.sarah5@yahoo.com>
2017-05-06 01:11:21 -0500

    my work menu javascript

--- js/scripts.js
+++ js/scripts.js
@@ -47,8 +47,11 @@ homepageLogosSwitch();
 
 
 
-var aboutMe = document.querySelector('#aboutme');
 var homepage = document.querySelector('#homepage');
+var aboutMe = document.querySelector('#aboutme');
+var myWork = document.querySelector('#mywork');
+var myWorkMenuAppend = document.querySelector('#mywork_append');
+var myWorkMenuAppendSpan = document.querySelectorAll('#mywork_append > span');
 var desktopMenuSection = document.querySelector('#desktop_menu_section');
 var desktopMenuLink = document.querySelectorAll('#menu a');
 
@@ -131,12 +134,10 @@ window.addEventListener("scroll", function(){
 		desktopSocialImages[4].setAttribute('src','images/social/twitter_black.png');
 		desktopSocialImages[5].setAttribute('src','images/social/fb_black.png');
 		desktopSocialImages[6].setAttribute('src','images/social/yt_black.png');
-
-		console.log('homepage');
 	
-	} else if (window.scrollY >= aboutMe.offsetTop) {
+	} else if(window.scrollY >= aboutMe.offsetTop && window.scrollY < myWork.offsetTop) {
 	
-		// ABOUT ME HOMEPAGE
+		// ABOUT ME MENU
 		desktopMenuSection.style.backgroundColor = "#E8E8E0";
 
 		for (var i = 0; i < desktopMenuLink.length; i++) {
@@ -151,6 +152,15 @@ window.addEventListener("scroll", function(){
 			desktopMenuLink[0].style.textShadow = "none";
 		});
 
+		desktopMenuLink[1].setAttribute('class','');
+		desktopMenuLink[1].style.textShadow = "-1px -1px 0 #F05559, 1px -1px 0 #F05559, -1px 1px 0 #F05559, 1px 1px 0 #F05559";
+		desktopMenuLink[1].addEventListener("mouseout", function(){
+			for (var i = 1; i < desktopMenuLink.length; i++) {
+				desktopMenuLink[1].style.color = "#E8DBD2";
+				desktopMenuLink[1].style.textShadow = "-1px -1px 0 #F05559, 1px -1px 0 #F05559, -1px 1px 0 #F05559, 1px 1px 0 #F05559";
+			}
+		});
+
 		upperLeftLogo.setAttribute('src','images/ar_logo_70px.png');
 
 		// ABOUT ME SOCIAL
@@ -162,7 +172,32 @@ window.addEventListener("scroll", function(){
 		desktopSocialImages[5].setAttribute('src','images/social/fb_pink.png');
 		desktopSocialImages[6].setAttribute('src','images/social/yt_pink.png');
 
-		console.log('about me');
+		myWorkMenuAppend.style.opacity = "0";
+		for (var i = 0; i < myWorkMenuAppendSpan.length; i++) {
+			myWorkMenuAppendSpan[i].style.marginTop = "-30px";
+		};
+
+	} else if(window.scrollY >= myWork.offsetTop){
+		desktopMenuSection.style.backgroundColor = "#E8DBD2";
+
+		for (var i = 0; i < desktopMenuLink.length; i++) {
+			desktopMenuLink[i].style.color = "#E8DBD2";
+		};
+
+		desktopMenuLink[0].setAttribute('class','');
+		desktopMenuLink[0].style.textShadow = "-1px -1px 0 #F05559, 1px -1px 0 #F05559, -1px 1px 0 #F05559, 1px 1px 0 #F05559";
+		desktopMenuLink[1].setAttribute('class','currentsection');
+		desktopMenuLink[1].style.color = "#F05559";
+		desktopMenuLink[1].style.textShadow = "none";
+		desktopMenuLink[1].addEventListener("mouseout", function(){
+			desktopMenuLink[1].style.color = '#F05559';
+			desktopMenuLink[1].style.textShadow = "none";
+		});
+
+		myWorkMenuAppend.style.opacity = "1";
+		for (var i = 0; i < myWorkMenuAppendSpan.length; i++) {
+			myWorkMenuAppendSpan[i].style.marginTop = "0px";
+		};
 	}
 });
 
@@ -249,6 +284,32 @@ var cameraPopup = document.querySelector('#camera_popup');
 		var i = 1
 		if(i == 1){
 			cameraPopup.style.opacity = "0";
+		}
+	});
+
+// PLANET EASTER EGG
+var planetIcon = document.querySelector('#planet');
+var planetPopup = document.querySelector('#planet_popup');
+	planetIcon.addEventListener("mouseover",function(){
+		console.log('planet');
+	})
+	planetIcon.addEventListener("click",function(){
+		planetPopup.style.opacity = i
+		var i = 0
+		if(i == 0){
+			planetPopup.style.opacity = "1";
+			i = 1;
+		} 
+		else if(i == 1){
+		 	planetPopup.style.opacity = "0";
+		 	i = 0;
+		}
+	});
+	planetPopup.addEventListener("click",function(){
+		planetPopup.style.opacity = i
+		var i = 1
+		if(i == 1){
+			planetPopup.style.opacity = "0";
 		}
 	});
 
