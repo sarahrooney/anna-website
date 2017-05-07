592c056d40508cad10562e7d08162ad00d865678
ennu0 <rooney.sarah5@yahoo.com>
2017-05-07 18:55:47 -0500

    transition btwn agency -> influencer

--- js/scripts.js
+++ js/scripts.js
@@ -178,7 +178,10 @@ window.addEventListener("scroll", function(){
 		};
 
 	} else if(window.scrollY >= myWork.offsetTop){
-		desktopMenuSection.style.backgroundColor = "#E8DBD2";
+
+		// MY WORK MENU
+		var myWorkMenuColor = "#E8DBD2";
+		desktopMenuSection.style.backgroundColor = myWorkMenuColor;
 
 		for (var i = 0; i < desktopMenuLink.length; i++) {
 			desktopMenuLink[i].style.color = "#E8DBD2";
@@ -198,27 +201,81 @@ window.addEventListener("scroll", function(){
 			desktopMenuLink[1].style.textShadow = "none";
 		});
 
+		// MY WORK EXTRA MENU EXPAND
 		myWorkMenuAppend.style.opacity = "1";
 		for (var i = 0; i < myWorkMenuAppendSpan.length; i++) {
 			myWorkMenuAppendSpan[i].style.marginTop = "0px";
 		};
 
+		// MY WORK PHOTO/TEXT SCROLL
 		var myWorkPhoto = document.querySelector('#mywork_photo');
+		var workContainer = document.querySelector('#work_container');
+		var workContainerChildren = document.querySelectorAll('#work_container > section');
+		var agency = document.querySelector('#agency');
+		var influencer = document.querySelector('#influencer');
+
 		myWorkMenuAppendSpan[0].addEventListener("click", function(){
+			myWork.style.backgroundColor = "#E8DBD2"
+			desktopMenuSection.style.backgroundColor = "#E8DBD2"
+				desktopMenuLink[0].style.color = "#E8DBD2"; //link color
+				desktopMenuLink[2].style.color = "#E8DBD2";
 			myWorkPhoto.style.backgroundPosition = "0% 55%"
+			agency.style.marginLeft = "0px"
+			upperLeftLogo.setAttribute('src','images/ar_logo_70px.png'); //blue logo
+
+			// planet easter egg
+			planetIcon.style.marginLeft = "85%";
+
+			//book easter egg disappear
+			bookIcon.style.marginLeft = "150%";
+			bookPopup.style.opacity = "0";
 		});
+
 		myWorkMenuAppendSpan[1].addEventListener("click", function(){
-			myWorkPhoto.style.backgroundPosition = "50% 55%"
+			myWork.style.backgroundColor = "#2230A4"
+			desktopMenuSection.style.backgroundColor = "#2230A4"
+				desktopMenuLink[0].style.color = "#2230A4"; //link color
+				desktopMenuLink[0].addEventListener("mouseout", function(){
+					desktopMenuLink[0].style.color = '#2230A4';
+				});
+				desktopMenuLink[2].style.color = "#2230A4";
+				desktopMenuLink[2].addEventListener("mouseout", function(){
+					desktopMenuLink[2].style.color = '#2230A4';
+				});
+			myWorkPhoto.style.backgroundPosition = "50% 55%";
+			agency.style.marginLeft = "-74%";
+			upperLeftLogo.setAttribute('src','images/ar_logo_70px_white.png'); //white logo
+
+			//planet easter egg disappear
+			planetIcon.style.marginLeft = "-30%";
+			planetPopup.style.opacity = "0";
+
+			//book easter egg appear
+			bookIcon.style.marginLeft = "70%";
 		});
+
 		myWorkMenuAppendSpan[2].addEventListener("click", function(){
+			myWork.style.backgroundColor = "#E8DBD2"
+			desktopMenuSection.style.backgroundColor = "#E8DBD2"
+				desktopMenuLink[0].style.color = "#E8DBD2"; //link color
+				desktopMenuLink[2].style.color = "#E8DBD2";
 			myWorkPhoto.style.backgroundPosition = "100% 55%"
+			agency.style.marginLeft = "-100%";
+			upperLeftLogo.setAttribute('src','images/ar_logo_70px.png'); //blue logo
+
+			//planet easter egg disappear
+			planetIcon.style.marginLeft = "-30%";
+			planetPopup.style.opacity = "0";
+			//book easter egg disappear
+			bookIcon.style.marginLeft = "150%";
+			bookPopup.style.opacity = "0";
 		});
 	}
 });
 
 
 
-//MENU CLICK
+//MENU CLICK AUTO SCROLL
 desktopMenuLink[0].addEventListener("click", function(){ 
 	if (window.scrollY < aboutMe.offsetTop){
 		var i = window.scrollY;
@@ -253,9 +310,6 @@ desktopMenuLink[0].addEventListener("click", function(){
 // FLOWER EASTER EGG
 var flowerIcon = document.querySelector('#flowericon');
 var flowerPopup = document.querySelector('#flower_popup');
-	flowerIcon.addEventListener("mouseover",function(){
-		console.log('flower');
-	})
 	flowerIcon.addEventListener("click",function(){
 		flowerPopup.style.opacity = i
 		var i = 0
@@ -279,9 +333,6 @@ var flowerPopup = document.querySelector('#flower_popup');
 // CAMERA EASTER EGG
 var cameraIcon = document.querySelector('#camera');
 var cameraPopup = document.querySelector('#camera_popup');
-	cameraIcon.addEventListener("mouseover",function(){
-		console.log('camera');
-	})
 	cameraIcon.addEventListener("click",function(){
 		cameraPopup.style.opacity = i
 		var i = 0
@@ -305,9 +356,6 @@ var cameraPopup = document.querySelector('#camera_popup');
 // PLANET EASTER EGG
 var planetIcon = document.querySelector('#planet');
 var planetPopup = document.querySelector('#planet_popup');
-	planetIcon.addEventListener("mouseover",function(){
-		console.log('planet');
-	})
 	planetIcon.addEventListener("click",function(){
 		planetPopup.style.opacity = i
 		var i = 0
@@ -325,6 +373,32 @@ var planetPopup = document.querySelector('#planet_popup');
 		var i = 1
 		if(i == 1){
 			planetPopup.style.opacity = "0";
+		}
+	});
+
+// BOOK EASTER EGG
+var bookIcon = document.querySelector('#book');
+var bookPopup = document.querySelector('#book_popup');
+	bookIcon.addEventListener("mouseover",function(){
+		console.log('book');
+	})
+	bookIcon.addEventListener("click",function(){
+		bookPopup.style.opacity = i
+		var i = 0
+		if(i == 0){
+			bookPopup.style.opacity = "1";
+			i = 1;
+		} 
+		else if(i == 1){
+		 	bookPopup.style.opacity = "0";
+		 	i = 0;
+		}
+	});
+	bookPopup.addEventListener("click",function(){
+		bookPopup.style.opacity = i
+		var i = 1
+		if(i == 1){
+			bookPopup.style.opacity = "0";
 		}
 	});
 
